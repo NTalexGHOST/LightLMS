f2ecf78d7600fc4d591dae27d08965bf367b184a
NTalexGHOST <alexpaa21@gmail.com>
2023-04-11 23:21:17 +0300

    Динамичные url для дисциплин теперь содержат их id, а не названия

--- src/main/java/ru/darkalive/LightLMS/controllers/view/ViewSubjectController.java
+++ src/main/java/ru/darkalive/LightLMS/controllers/view/ViewSubjectController.java
@@ -5,10 +5,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import ru.darkalive.LightLMS.entities.Subject;
 import ru.darkalive.LightLMS.entities.User;
-import ru.darkalive.LightLMS.repos.GroupRepository;
-import ru.darkalive.LightLMS.repos.RoleRepository;
-import ru.darkalive.LightLMS.repos.UserRepository;
+import ru.darkalive.LightLMS.repos.*;
 
 @Controller
 public class ViewSubjectController {
@@ -19,6 +19,23 @@ public class ViewSubjectController {
     private UserRepository userRepo;
     @Autowired
     private RoleRepository roleRepo;
+    @Autowired
+    private SubjectRepository subjectRepo;
+    @Autowired
+    private LinkUserSubjectRepository linkUserSubjectRepo;
+
+
+    @GetMapping("/subjects/{subjectId}")
+    public String subject(Model model, @PathVariable("subjectId") int subjectId) throws Exception {
+
+        User authorizedUser = userRepo.findFirstByUserName(SecurityContextHolder.getContext().getAuthentication().getName());
+        model.addAttribute("authorizedUser", authorizedUser);
+
+        Subject subject = subjectRepo.findFirstById(subjectId);
+        model.addAttribute("subject", subject);
+
+        return "subject";
+    }
 
     @GetMapping("/example")
     public String example(Model model) throws Exception {
