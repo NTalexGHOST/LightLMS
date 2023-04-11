05699bc5463c503a10d47dcfe816eb4802a1bc2e
NTalexGHOST <alexpaa21@gmail.com>
2023-04-11 23:21:43 +0300

    Дополнение к предыдущему коммиту

--- src/main/java/ru/darkalive/LightLMS/repos/SubjectRepository.java
+++ src/main/java/ru/darkalive/LightLMS/repos/SubjectRepository.java
@@ -11,4 +11,5 @@ import java.util.List;
 public interface SubjectRepository extends CrudRepository<Subject, Integer> {
 
     List<Subject> findAllByTeacher(User user);
+    Subject findFirstById(int id);
 }
